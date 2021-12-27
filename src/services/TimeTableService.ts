74ba71892e98aa4e482af907d46580201218bc1b
bepyan <65283190+bepyan@users.noreply.github.com>
2021-12-27 22:09:52 +0900

    fix. transfer time

--- src/services/TimeTableService.ts
+++ src/services/TimeTableService.ts
@@ -20,20 +20,20 @@ export const getStationTimeTable = async (params: TimeTableParams) => {
 
 /* ----------------  ---------------- */
 
-const transerTime = (time: string | number) => {
+const transferTime = (time: string | number) => {
   const target = time + "";
   if (target.length < 6) return "00:00:00";
   const HH = target.substring(0, 2);
-  const MM = target.substring(0, 2);
-  const SS = target.substring(0, 2);
+  const MM = target.substring(2, 4);
+  const SS = target.substring(4, 6);
 
   return `${HH}:${MM}:${SS}`;
 };
 
 export const transferTimeTable = (timeTable: TimeTableResponse): I시간표 => {
   return {
-    도착시간: transerTime(timeTable.arrTime),
-    출발시간: transerTime(timeTable.depTime),
+    도착시간: transferTime(timeTable.arrTime),
+    출발시간: transferTime(timeTable.depTime),
     요일구분: dailyTypeMapper[timeTable.dailyTypeCode],
     상하행구분: upDownTypeMapper[timeTable.upDownTypeCode],
     종점역_ID: timeTable.endSubwayStationId,
